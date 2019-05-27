8ccd4adc95361db3f4b3c69dae73b730fdc9bf5f
annie tran <nntrn@me.com>
2019-05-27 12:02:40 -0500

    clean up divs from isUserLoggedIn()

--- client/src/components/MenuComponent.js
+++ client/src/components/MenuComponent.js
@@ -7,57 +7,48 @@ class MenuComponent extends Component {
   render() {
     const isUserLoggedIn = AuthenticationService.isUserLoggedIn()
     return (
-      <>
-        <div className="container">
-          <h1 className="header">Title here</h1>
-          <div>
-            <ul>
-              {isUserLoggedIn && (
-                <li>
-                  <Link to="/clients">clients</Link>
-                </li>
-              )}
-              {isUserLoggedIn && (
-                <li>
-                  <Link to="/pets">pets</Link>
-                </li>
-              )}
-              {isUserLoggedIn && (
-                <li>
-                  <Link to="/appointments">appointments</Link>
-                </li>
-              )}
-            </ul>
-            <ul>
-              {isUserLoggedIn && (
-                <span role="button">{AuthenticationService.getLoggedInUserName()}</span>
-              )}
-              {isUserLoggedIn && (
-                <>
-                  <Link to="/profile">
-                    <Icon type="icon-user-circle-o" textAfter="Profile" />
-                  </Link>
-                  <Link to="/settings">
-                    <Icon type="icon-cog" textAfter="Settings" />
-                  </Link>
-                  <Link to="/logout" title="logout" onClick={AuthenticationService.logout}>
-                    <Icon type="icon-logout" textAfter="Sign out" />
-                  </Link>
-                </>
-              )}
-            </ul>
-          </div>
+      <div className="container">
+        <h1 className="header">Title here</h1>
 
-          <div>
-            {/* LOGIN LINK (show only if user is not logged in) */}
-            {!isUserLoggedIn && (
-              <Link to="/login">
-                <Icon type="icon-login" textBefore="Login" />
+        {isUserLoggedIn && (
+          <ul>
+            <li>
+              <Link to="/clients">clients</Link>
+            </li>
+            <li>
+              <Link to="/pets">pets</Link>
+            </li>
+            <li>
+              <Link to="/appointments">appointments</Link>
+            </li>
+          </ul>
+        )}
+
+        <ul>
+          {isUserLoggedIn && (
+            <>
+              <span role="button">{AuthenticationService.getLoggedInUserName()}</span>
+              <Link to="/profile">
+                <Icon type="icon-user-circle-o" textAfter="Profile" />
               </Link>
-            )}
+              <Link to="/settings">
+                <Icon type="icon-cog" textAfter="Settings" />
+              </Link>
+              <Link to="/logout" title="logout" onClick={AuthenticationService.logout}>
+                <Icon type="icon-logout" textAfter="Sign out" />
+              </Link>
+            </>
+          )}
+        </ul>
+
+        {!isUserLoggedIn && (
+          <div>
+            <Link to="/login">
+              <Icon type="icon-login" textBefore="Login" />
+            </Link>
           </div>
-        </div>
-      </>
+        )}
+      </div>
     )
   }
 }
